c5e43f7366664e2730bf25eb0dfeca7532c06948
Marvin Li <marvinli@gmail.com>
2022-10-15 09:21:45 -0400

    Render Moby Dick

---
+++ src/hooks/useViewportTracker.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react';
+
+const useViewportTracker = (ref) => {
+  const [isVisible, setIsVisible] = useState(false);
+  // const [scrollFraction, setScrollFraction] = useState(0);
+  const [topFraction, setTopFraction] = useState(0);
+
+  useEffect(() => {
+    if (!ref.current) {
+      return;
+    }
+    
+    const observer = new IntersectionObserver(([entry]) => {
+      const { isIntersecting } = entry;
+      setIsVisible(isIntersecting);
+    });
+    observer.observe(ref.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const boundingRect = ref.current.getBoundingClientRect();
+      const distanceTop = boundingRect.top;
+      const height = window.innerHeight;
+      const topFraction = Math.min(
+        Math.max(1 - distanceTop / height, 0),
+        1
+      );
+      setTopFraction(topFraction);
+    };
+    if (isVisible) {
+      window.addEventListener("scroll", handleScroll);
+    } else {
+      window.removeEventListener("scroll", handleScroll);
+    }
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    }
+  }, [ref, isVisible])
+
+  if (!isVisible) {
+    return {
+      isVisible,
+      // scrollFraction: 0,
+      topFraction: 0
+    }
+  }
+
+  return {
+    isVisible,
+    // scrollFraction,
+    topFraction,
+  };
+};
+
+export default useViewportTracker;
